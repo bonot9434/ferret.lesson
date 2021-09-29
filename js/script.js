585462e29917c5a3c36c9a2b4adb467e64aec909
bonot9434 <bonot9434@gmail.com>
2021-09-29 23:55:05 +0900

    別スライドショー

--- js/script.js
+++ js/script.js
@@ -19,5 +19,34 @@ $(".theTarget").skippr({
   // キーボードの矢印キーによるスライド送りの設定（trueで有効）
   keyboardOnAlways : true,
   // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-  hidePrevious : false
+  hidePrevious : false,
 });
+
+
+$(".slider").slick({
+  //次のスライド表示
+  centerMode: true,
+  //表示スライドの余白
+  centerPadding: '10%',
+  //インジケーター表示
+  dots: true,
+  //自動再生
+  autoplay: true,
+  //自動再生速度
+  autoplaySpeed: 3000,
+  //フェードアニメーションの速度
+  speed: 1000,
+  //スライドのループ
+  infinite: true,
+});
+
+
+
+// $('.slider').slick({
+//   // 自動再生を設定
+//   autoplay:true,
+//   // スライド切り替えの時間を設定
+//   autoplaySpeed: 3000,
+//   // インジケーターを表示
+//   dots:true
+// });
